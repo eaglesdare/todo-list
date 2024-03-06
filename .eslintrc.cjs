a95a2a97209af174c959e064bfeaef88b6b22c41
Anton Pivovarov <90635382+eaglesdare@users.noreply.github.com>
2024-03-06 17:20:46 +0200

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['@wemake-services/typescript/recommended'],
+    }
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+
+const App: FunctionComponent = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default App
